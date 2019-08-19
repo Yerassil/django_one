1287905ec512e21253d385cc981c46a1398cf6e9
Yerassil <yerassil@gmail.com>
2019-08-19 16:44:38 +0600

    Add exception when ordering more items than available in stock

--- django_one/models.py
+++ django_one/models.py
@@ -42,6 +42,8 @@ class Order(models.Model):
                 store=store,
                 product=item.product
             )
+            if item.quantity > store_item.quantity:
+                raise Exception('Not enough stock')
             store_item.quantity -= item.quantity
             store_item.save()
 
