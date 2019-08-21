8e60261fa7fce91c9d1bf590ff3f8a914395dfec
Yerassil <yerassil@gmail.com>
2019-08-22 01:32:55 +0600

    Add customer-order link

--- django_one/models.py
+++ django_one/models.py
@@ -31,6 +31,10 @@ class StoreItem(models.Model):
 
 
 class Order(models.Model):
+    customer = models.ForeignKey(
+        Customer, on_delete=models.CASCADE,
+        related_name='orders'
+    )
     location = models.CharField(max_length=100, blank=True)
     price = models.DecimalField(
         max_digits=10, decimal_places=2,
